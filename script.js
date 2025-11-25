c563d89f2214bd6c1ec628c4ed568839abe2adbd
Karen Araujo <barbiekarenalves@outlook.com>
2025-11-25 11:28:17 -0300

    criando projeto encurtador de links

---
+++ script.js
@@ -0,0 +1,12 @@
+const input = document.querySelector("#link");
+const res = document.querySelector("#resultado");
+const botao = document.querySelector("#botao");
+
+botao.addEventListener('click', function(){
+    const linkOriginal = input.value;
+    const codigo = Math.random().toString(36).substring(2, 8);
+    const linkCurto = `https://encurta.me/${codigo}`
+
+    res.innerHTML = `<a href="${linkCurto}" target="_blank">${linkCurto}</a>`;
+
+})
